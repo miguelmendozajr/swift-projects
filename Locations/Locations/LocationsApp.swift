4ae8b33573a233e14c4ed90d9f3c0a8d52033598
miguelmendozajr <miguelmendozaj20@gmail.com>
2024-10-01 20:08:19 -0600

    created project

---
+++ Locations/Locations/LocationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationsApp.swift
+//  Locations
+//
+//  Created by Miguel Mendoza on 01/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LocationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
